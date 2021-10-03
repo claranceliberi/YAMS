c13dbd539856a756db5a3fa73833bad5df54f0c8
FlutLab.io <>
2021-10-03 06:13:02 +0000

    Autosave

--- lib/widgets/input_field.dart
+++ lib/widgets/input_field.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class InputField extends StatelessWidget {
-  const InputField({Key key}) : super(key: key);
+  // const InputField({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
