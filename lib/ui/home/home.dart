a3148a473b829b1e9be7f87bdc9587a65d5cdb46
FlutLab.io <>
2021-10-03 10:58:44 +0000

    Autosave

--- lib/ui/home/home.dart
+++ lib/ui/home/home.dart
@@ -11,19 +11,20 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      body: Container(
         child: Padding(
           padding: const EdgeInsets.all(20),
           child: Column(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
             Container(
                 child: Column(children: <Widget>[
               Image.asset("assets/images/app-logo.png"),
-              Container(
+              Padding(
                 padding: EdgeInsets.symmetric(vertical: 20),
                 child: Text("YAMS", textAlign: TextAlign.center, style: TextStyle(color: Color.fromRGBO(239, 68, 68, 1), fontSize: 32, fontWeight: FontWeight.w700)),
               ),
-              Text("You Are My Sunshine, App that gives you all information you need about solar and other non-renewable energy", textAlign: TextAlign.center, style: TextStyle(fontSize: 16))
-            ]))
+              Text("You Are My Sunshine, App that gives you all information you need about solar and other non-renewable energy.", textAlign: TextAlign.center, style: TextStyle(fontSize: 16))
+            ])),
+            Container(child: TextButton(child: Text("Get Started"), onPressed: () {}))
           ]),
         ),
       ),
