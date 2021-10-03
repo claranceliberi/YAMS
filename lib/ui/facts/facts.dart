4066d42dc952de4c8bac5940660dd4b5b746b03d
FlutLab.io <>
2021-10-03 15:44:18 +0000

    graph

--- lib/ui/facts/facts.dart
+++ lib/ui/facts/facts.dart
@@ -87,8 +87,8 @@ class _FactsState extends State<Facts> {
 
   List<EnergyData> getEnergyData() {
     final List<EnergyData> energyData = [
-      EnergyData("Electricity", 30, Color.fromRGBO(239, 68, 68, .5)),
-      EnergyData("Solar", 70, Color.fromRGBO(239, 68, 68, .9))
+      EnergyData("Electricity", 30, Color.fromRGBO(239, 68, 68, 0.3)),
+      EnergyData("Solar", 70, Color.fromRGBO(239, 68, 68, 0.9))
     ];
 
     return energyData;
