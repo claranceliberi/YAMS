63f1121d2a1fadfaa322423fc630043f18dc4deb
FlutLab.io <>
2021-10-03 13:48:44 +0000

    Autosave

--- lib/ui/welcome/welcome.dart
+++ lib/ui/welcome/welcome.dart
@@ -13,21 +13,18 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
       body: Container(
         child: Padding(
           padding: const EdgeInsets.all(20),
-          child: Column(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-            Expanded(
-                child: Center(
-                    child: Container(
-                        child: Column(children: <Widget>[
+          child: Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+            Container(
+                child: Column(children: <Widget>[
               Image.asset("assets/images/app-logo.png"),
               Padding(
                 padding: EdgeInsets.symmetric(vertical: 20),
                 child: Text("YAMS", textAlign: TextAlign.center, style: TextStyle(color: Color.fromRGBO(239, 68, 68, 1), fontSize: 32, fontWeight: FontWeight.w700)),
               ),
-              Text("Yoau Are My Sunshine, App that gives you all information you need about solar and other non-renewable energy.", textAlign: TextAlign.center, style: TextStyle(fontSize: 16))
-            ])))),
-            Expanded(
-                child: Container(
-                    child: Row(children: [
+              Text("You Are My Sunshine, App that gives you all information you need about solar and other non-renewable energy.", textAlign: TextAlign.center, style: TextStyle(fontSize: 16))
+            ])),
+            Container(
+                child: Row(children: [
               Expanded(
                   child: TextButton(
                       child: Text("Get Started"),
@@ -35,7 +32,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                         Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => Facts()));
                       },
                       style: TextButton.styleFrom(padding: EdgeInsets.symmetric(vertical: 24.0), primary: Colors.white, textStyle: TextStyle(color: Colors.white, fontSize: 18), backgroundColor: Color.fromRGBO(239, 68, 68, 1))))
-            ])))
+            ]))
           ]),
         ),
       ),
