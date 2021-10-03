778810203ca9fcd9e3d82b9da0711231767ea3d7
FlutLab.io <>
2021-10-03 10:50:23 +0000

    Autosave

--- lib/ui/home/home.dart
+++ lib/ui/home/home.dart
@@ -18,7 +18,10 @@ class _HomeScreenState extends State<HomeScreen> {
             Container(
                 child: Column(children: <Widget>[
               Image.asset("assets/images/app-logo.png"),
-              Text("YAMS", textAlign: TextAlign.center, style: TextStyle(color: Color.fromRGBO(239, 68, 68, 1), fontSize: 32, fontWeight: FontWeight.w700)),
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 20),
+                child: Text("YAMS", textAlign: TextAlign.center, style: TextStyle(color: Color.fromRGBO(239, 68, 68, 1), fontSize: 32, fontWeight: FontWeight.w700)),
+              ),
               Text("You Are My Sunshine, App that gives you all information you need about solar and other non-renewable energy", textAlign: TextAlign.center, style: TextStyle(fontSize: 16))
             ]))
           ]),
