34eaed764e63c59ab6e8c9123adf07a8bd016103
FlutLab.io <>
2021-10-03 12:12:48 +0000

    Autosave

--- lib/ui/home/welcome.dart
+++ lib/ui/home/welcome.dart
@@ -31,7 +31,7 @@ class _HomeScreenState extends State<HomeScreen> {
                       onPressed: () {
                         Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => Facts()));
                       },
-                      style: TextButton.styleFrom(padding: EdgeInsets.symmetric(vertical: 24.0), primary: Colors.white, textStyle: TextStyle(color: Colors.white, fontSize: 18), backgroundColor: Color.fromRGBO(239, 68, 68, 1), shape: MaterialStateProperty.all<RoundedRectangleBorder>(RoundedRectangleBorder(borderRadius: BorderRadius.circular(16.0))))))
+                      style: TextButton.styleFrom(padding: EdgeInsets.symmetric(vertical: 24.0), primary: Colors.white, textStyle: TextStyle(color: Colors.white, fontSize: 18), backgroundColor: Color.fromRGBO(239, 68, 68, 1))))
             ]))
           ]),
         ),
